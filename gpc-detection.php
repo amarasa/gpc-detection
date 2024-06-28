9cdb7f31fdb0bacc38e354b4c55c44d2d2b53ab8
Angelo Marasa <angelo.marasa@kaleidico.com>
2024-06-28 15:25:39 -0400

    Fixed updater

--- gpc-detection.php
+++ gpc-detection.php
@@ -6,7 +6,19 @@ Version: 1.0.1
 Plugin URI: https://github.com/amarasa/gpc-detection
 Author: Angelo Marasa
 */
-require_once plugin_dir_path(__FILE__) . 'includes/updater.php';
+require_once plugin_dir_path(__FILE__) . 'includes/require 'puc/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/amarasa/gpc-detection',
+    __FILE__,
+    'gpc-detection-plugin'
+);
+
+// Optional: If you're using a private repository, specify the access token like this:
+// $myUpdateChecker->setAuthentication('your-token-here');
+.php';
 
 function enqueue_gpc_script()
 {
